aa88ef6d9e8a63ca398942ef7ca72b35d8771e1a
jahoops <jwhooper@gmail.com>
2018-08-12 15:45:25 -0400

    with tests

--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace UnitTestProject1
 {
@@ -6,9 +7,42 @@ namespace UnitTestProject1
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckTypeReturn()
         {
-            bigramlib.biGrams("garbage");
+            var blib = new testenginterview.bigramlib();
+            string empty = "";
+            Assert.IsInstanceOfType(blib.biGrams(empty), typeof(List<string>));
+        }
+
+        [TestMethod]
+        public void IgnoreEmpty()
+        {
+            var blib = new testenginterview.bigramlib();
+            string empty = "";
+            Assert.AreEqual(blib.biGrams(empty).Count, 0);
+        }
+
+        [TestMethod]
+        public void IgnoreOneWord()
+        {
+            var blib = new testenginterview.bigramlib();
+            string oneword = "the";
+            Assert.AreEqual(blib.biGrams(oneword).Count, 0);
+        }
+
+        [TestMethod]
+        public void IgnoreGarbage()
+        {
+            var blib = new testenginterview.bigramlib();
+            string garbage = "~!@#$%^&*()_+{}|[]\\:\";'<>?,./ ~!@#$%^&*()_+{}|[]\\:\";'<>?,./";
+            Assert.AreEqual(blib.biGrams(garbage).Count, 0);
+        }
+
+        [TestMethod]
+        public void CountIncludingDuplicates()
+        {
+            var blib = new testenginterview.bigramlib();
+            Assert.AreEqual(blib.biGrams("“The quick brown fox and the quick blue hare.”").Count,8);
         }
     }
 }
--- testenginterview/Program.cs
+++ testenginterview/Program.cs
@@ -1,68 +1,14 @@
 ﻿using System;
 using System.Text;
 using System.Collections.Generic;
-using System.Linq;
 using System.IO;
 
 namespace testenginterview
 {
 
-    class bigramlib
+    public class bigramlib
     {
-        static void Main(string[] args)
-        {
-            if (args.Length < 1)
-            {
-                Console.WriteLine(@"No file to input: example( testenginterview C:\testfile )");
-                System.Console.ReadKey();
-                return;
-            }
-            try
-            {
-                string text = System.IO.File.ReadAllText(args[0]);
-                List<string> output = bigramlib.biGrams(text);
-                if (output.Count > 0)
-                {
-                    output.Sort();
-                    var lastKey = output[0];
-                    var lastCount = 0;
-                    foreach (var bigram in output)
-                    {
-                        if (bigram == lastKey)
-                        {
-                            lastCount++;
-                        }
-                        else
-                        {
-                            Console.WriteLine("\"{0}\" {1}", lastKey, lastCount);
-                            lastKey = bigram;
-                            lastCount = 1;
-                        }
-                    }
-                    Console.WriteLine("\"{0}\" {1}", lastKey, lastCount);
-                }
-                else
-                {
-                    Console.WriteLine("no bigrams found");
-                }
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine(@"File {0} not found", args[0]);
-            }
-            catch (IOException e)
-            {
-                Console.WriteLine(@"Error reading file {0}", args[0]);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(@"Error : {0}", e);
-            }
-            Console.WriteLine("Press any key to exit.");
-            System.Console.ReadKey();
-        }
-
-        public static List<string> biGrams(string text)
+        public List<string> biGrams(string text)
         {
             List<string> biGrams = new List<string>();
             string saveLast = "";
@@ -108,6 +54,60 @@ namespace testenginterview
                 }
             }
             return biGrams;
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine(@"No file to input: example( testenginterview C:\testfile )");
+                System.Console.ReadKey();
+                return;
+            }
+            try
+            {
+                string text = System.IO.File.ReadAllText(args[0]);
+                bigramlib bgl = new bigramlib();
+                List<string> output = bgl.biGrams(text);
+                if (output.Count > 0)
+                {
+                    output.Sort();
+                    var lastKey = output[0];
+                    var lastCount = 0;
+                    foreach (var bigram in output)
+                    {
+                        if (bigram == lastKey)
+                        {
+                            lastCount++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("\"{0}\" {1}", lastKey, lastCount);
+                            lastKey = bigram;
+                            lastCount = 1;
+                        }
+                    }
+                    Console.WriteLine("\"{0}\" {1}", lastKey, lastCount);
+                }
+                else
+                {
+                    Console.WriteLine("no bigrams found");
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(@"File {0} not found", args[0]);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(@"Error reading file {0}", args[0]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(@"Error : {0}", e);
+            }
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
         }
     }
 
